a2a70a9f24af15faadf4b4aedbf4802e6ee75aae
G-Mounika345 <144306947+G-Mounika345@users.noreply.github.com>
2025-06-14 13:37:27 +0530

    Initial commit - .NET SCA Demo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        var person = new { Name = "Alice", Age = 30 };
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine(json);
+    }
+}
+
+
